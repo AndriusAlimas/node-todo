b691d3c8d82b802411a6efb6adb6f95f27981af3
Andrius Alimas <XandrejoX@yahoo.com>
2022-10-27 14:59:22 +0100

    added encryption and decryption password and other config details for mongoose to able connect to database

--- config/index.js
+++ config/index.js
@@ -1,5 +1,12 @@
 var configValues = require("./config");
+const Cryptr = require("cryptr");
+const cryptr = new Cryptr("myTotallySecretKey");
+
+const encryptedPwd = cryptr.encrypt(configValues.pwd);
+const decryptedPwd = cryptr.decrypt(encryptedPwd);
 
 module.exports = {
-  getDbConnectionString: function () {},
+  getDbConnectionString: function () {
+    return `mongodb+srv://${configValues.username}:${decryptedPwd}@cluster0.mhwtq3a.mongodb.net/${configValues.databaseName}`;
+  },
 };
