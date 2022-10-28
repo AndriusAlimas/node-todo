f113e23517cc5b417492e5c59556c53676512707
Andrius Alimas <XandrejoX@yahoo.com>
2022-10-28 11:58:14 +0100

    created method to find todo using id

--- controllers/apiController.js
+++ controllers/apiController.js
@@ -5,11 +5,19 @@ module.exports = function (app) {
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({ extended: true }));
 
+  // find the todos using username
   app.get("/api/todos/:user", function (req, res) {
-    // find that person
     Todos.find({ username: req.params.user }, function (err, todos) {
       if (err) throw err;
       res.send(todos);
+    });
+  });
+
+  // find the todo using id
+  app.get("/api/todo/:id", function (req, res) {
+    Todos.findById({ _id: req.params.id }, function (err, todo) {
+      if (err) throw err;
+      res.send(todo);
     });
   });
 };
